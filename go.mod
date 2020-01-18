a9481976df2fbd683fcb5ed865efac62b7499556
Sebastian Seggewiss <sebastianseggewiss@gmail.com>
2020-01-18 08:46:55 +0100

    NTR - Enable go mod + fix pipeline

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/King-of-Babylon/go-google-hashcode
+
+go 1.13
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "github.com/King-of-Babylon/hashcode2020/loader"
-    "github.com/King-of-Babylon/hashcode2020/processor"
-    "github.com/King-of-Babylon/hashcode2020/writer"
+    "github.com/King-of-Babylon/go-google-hashcode/pkg/loader"
+    "github.com/King-of-Babylon/go-google-hashcode/pkg/processor"
+    "github.com/King-of-Babylon/go-google-hashcode/pkg/writer"
 )
 
 func main() {
     filenames := loader.LoadFilesToProcess()
 
     for _, filename := range filenames {
-        config, data := loader.Load("./src/" + filename)
+        config, data := loader.Load("./input/" + filename)
         result := processor.Process(config, data)
         writer.Write(result, filename)
     }
